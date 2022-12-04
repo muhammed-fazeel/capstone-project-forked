748db8a9ebfd48f0ab1c597a0f6269d0bbace746
Dhruv Kanthaliya <dhruv.kanthaliya@happiestminds.com>
2022-12-05 00:18:25 +0530

    Added Angular Template

--- Upskill/UpSkillWebApi/UpSkillWebApi/Models/IDatabase.cs
+++ Upskill/UpSkillWebApi/UpSkillWebApi/Models/IDatabase.cs
@@ -2,8 +2,42 @@
 {
     public interface IDatabase
     {   
-        void AddCategory(Category category);
-        List<Category> GetAllCategories();
+
+        //calls for courses 
+        public void AddCourse(Course course);  
         List<Course> GetAllCourses();
+
+        public Course GetCourse(int id);    
+        public List<Course> GetCoursesByCategory(int CatId);
+        public void DeleteCourse(int CourseId);
+
+        //calls for Categories
+        List<Category> GetAllCategories();
+
+        void AddCategory(Category category);
+
+
+        //calls for reviews
+        public List<Review> GetReviews();
+        public void AddReview(Review review);   
+
+        //calls for videoLinks
+        public List<VideoLinks> GetVideoLinksByCourseId(int CourseId);
+        public void AddVideo(VideoLinks video); 
+        public void DeleteVideo(int VideoId);
+
+        //calls for enrolled courses
+        public List<EnrolledCourses> GetAllEnrolledCoursesByUserId(int id);
+
+        public void AddCourse(EnrolledCourses course,int UserId);  // adding the course to user's enrolled courses 
+
+        //calls for Users- this will used for authentication
+        // to-do add the functions here
+
+
+        //calls for Roles
+        public Role GetRole(int UserId);
+
+        
     }
 }
--- Upskill/UpSkillWebApi/UpSkillWebApi/Models/UpSkillEFDAO.cs
+++ Upskill/UpSkillWebApi/UpSkillWebApi/Models/UpSkillEFDAO.cs
@@ -13,6 +13,36 @@
             throw new NotImplementedException();
         }
 
+        public void AddCourse(Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCourse(EnrolledCourses course, int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddReview(Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddVideo(VideoLinks video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCourse(int CourseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteVideo(int VideoId)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Category> GetAllCategories()
         {
             var lstCats = dbCtx.Category.ToList();
@@ -23,6 +53,36 @@
         {
             var lstCourses = dbCtx.Course.ToList();
             return lstCourses;
+        }
+
+        public List<EnrolledCourses> GetAllEnrolledCoursesByUserId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course GetCourse(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Course> GetCoursesByCategory(int CatId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Review> GetReviews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Role GetRole(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<VideoLinks> GetVideoLinksByCourseId(int CourseId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
