c1c922484ecdfb5faac54e616ec7dee113788fee
fazeel <muhamed.faseel@happiestminds.com>
2022-12-21 13:44:32 +0530

    Changes to course-page

--- Angular-Components/UpSkillUi/src/app/components/course-page/course-page.component.ts
+++ Angular-Components/UpSkillUi/src/app/components/course-page/course-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { Course } from 'src/app/models/Course';
+import { Review } from 'src/app/models/Review';
 import { VideoLinks } from 'src/app/models/VideoLinks';
 import { UpskillService } from 'src/app/services/upskill.service';
 
@@ -10,10 +12,12 @@ import { UpskillService } from 'src/app/services/upskill.service';
   styleUrls: ['./course-page.component.css']
 })
 export class CoursePageComponent implements OnInit {
-  catId:number=1;
+  courseId:number=5;
   textclass='text';
+  course=new Course(1,"",1,"",1,1,1,"","");
   coursetitle:string="";
   videolinks:VideoLinks[]=[];
+  reviews:Review[]=[];
   present_video:SafeResourceUrl="https://www.youtube.com/watch?v=nOY0TWWvynU";
   constructor(private upskillservice:UpskillService,public sanitizer:DomSanitizer,private activatedRoute:ActivatedRoute) { }
 
@@ -22,11 +26,15 @@ export class CoursePageComponent implements OnInit {
         // console.log(params);
         let catId = params["id"];
       });
-    this.upskillservice.GetVideoLinksByCourseId(this.catId).subscribe(data=>{
+    this.upskillservice.GetVideoLinksByCourseId(this.courseId).subscribe(data=>{
       this.videolinks=data;
       // console.log(this.videolinks);
-
-      
+    });
+    this.upskillservice.getReviews(this.courseId).subscribe(data=>{
+      this.reviews=data;
+    });
+    this.upskillservice.getCourseById(this.courseId).subscribe(data=>{
+      this.course=data;
     });
   }
   showVideo(v:VideoLinks){
